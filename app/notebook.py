0e9be74ac46a2cab3a11c3a1e4b43c3235c1bd83
David123Taborda <dtaborda789@soyudemedellin.edu.co>
2025-02-18 11:12:28 -0500

    se creo la clase Note y se definieron los parametros, tambien se establecieron los metodos.
    se creo la clase Notebook con todos sus metodos y parametros.

--- app/notebook.py
+++ app/notebook.py
@@ -20,3 +20,30 @@ class Note:
     def __str__(self):
         return f"Date: {self.creation_date}\n{self.title}: {self.text}"
 
+
+class Notebook:
+    def __init__(self):
+        self.notes = []
+
+    def add_note(self, title: str, text: str, importance: str) -> int:
+        code = len(self.notes) + 1
+        new_note = Note(code, title, text, importance)
+        self.notes.append(new_note)
+        return code
+
+    def delete_note(self, code: int):
+        self.notes = [note for note in self.notes if note.code != code]
+
+    def important_notes(self) -> list[Note]:
+        return [note for note in self.notes if note.importance in (Note.HIGH, Note.MEDIUM)]
+
+    def notes_by_tag(self, tag: str) -> list[Note]:
+        return [note for note in self.notes if tag in note.tags]
+
+    def tag_with_most_notes(self) -> str:
+        tag_counts = Counter(tag for note in self.notes for tag in note.tags)
+        if not tag_counts:
+            return ""
+        return min(tag_counts, key=lambda tag: (-tag_counts[tag], tag))
+
+
